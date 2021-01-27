f1f2a98807a2228ab3966473c05e6de59c100a31
amcgaw07 <44219088+amcgaw07@users.noreply.github.com>
2021-01-26 23:11:56 -0500

    charset

--- myscript.js
+++ myscript.js
@@ -11,7 +11,7 @@ var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
   type: 'horizontalBar',
   data: {
-      labels: ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7","Team 8","Team 9","Team 10","Team 11","Team 12"],
+      labels: ["ΑΔΠ, ΦΔΘ, ΑΦA", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7","Team 8","Team 9","Team 10","Team 11","Team 12"],
       datasets: [
           {
                label: "Scavenger Hunt",
